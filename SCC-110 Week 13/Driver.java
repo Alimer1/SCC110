0d0d5e4feace90cc998dd2b88a0f11ee33de70ea
Ali Omer Misirli <aliomermsrl@gmail.com>
2022-02-24 16:53:40 +0000

    For some reason I couln't draw a ball so I gave up

---
+++ SCC-110 Week 13/Driver.java
@@ -0,0 +1,8 @@
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        GameArena MainGame = new GameArena(1000,1000);
+        Ball RedBall = new Ball(500.0,500.0,200.0,"BLUE",5);
+    }
+}
