02c7b0fbd5d705ee0ceaeef5da8c530e11b93073
Ali Omer Misirli <aliomermsrl@gmail.com>
2022-03-20 06:54:23 +0000

    4. Commit I am goint to sleep or I will collapse

--- SCC-110 Week 18 Assesed Exercise/GameScreen.java
+++ SCC-110 Week 18 Assesed Exercise/GameScreen.java
@@ -4,32 +4,35 @@ import java.awt.event.*;
 
 public class GameScreen
 {
+    private int numberOfColours;    //Number Of Colours
     private int secretCodeLenght;   //Lenght of the secret code
     private int position = 0;       //Position of the target from left to right 0-1-2-3-...
     private int correct = 0;        //How many correct guesses have been made in the correct location
     private int halfCorrect = 0;    //How many correct guesses have been made in the wrong location
-    private int secretCode;         //Answer of the game
+    private int secretCode[];         //Answer of the game
     private JPanel gamePanel;       //Panel that will be sent to the GameController
     private JLabel labels[];        //Array of icons
 
 
-    public GameScreen(int newSecretCodeLenght) //If you are using this constructor then this is the first game screen and you will later request the secretCode from this game screen
+    public GameScreen(int newSecretCodeLenght,int newNumberOfColours) //If you are using this constructor then this is the first game screen and you will later request the secretCode from this game screen
     {
+        numberOfColours = newNumberOfColours;
         secretCodeLenght = newSecretCodeLenght;
+        secretCode = new int[secretCodeLenght]; //I lost too much IQ while figuring this out
         gamePanel.setLayout(new GridLayout(1,secretCodeLenght));
-        double n = Math.random()*(secretCodeLenght^10);
-        secretCode = (int) n;
-
-        for(int i=0; i<secretCodeLenght; i++)
+        for(int i=0; i<secretCodeLenght;i++)
         {
+            double n = Math.random()*(secretCodeLenght^10);
+            secretCode[i] = (int) n;
             labels[i] = new JLabel(new Picture("Empty.png"));
             gamePanel.add(labels[i]);
         }
     }
     
 
-    public GameScreen(int newSecretCode,int newSecretCodeLenght)
+    public GameScreen(int newSecretCodeLenght,int newNumberOfColours,int newSecretCode)
     {
+        numberOfColours = newNumberOfColours;
         secretCodeLenght = newSecretCodeLenght;
         gamePanel.setLayout(new GridLayout(1,secretCodeLenght));
         secretCode = newSecretCode;
@@ -41,19 +44,43 @@ public class GameScreen
         }
     }
 
+    private void correctionCalculator(int colour)   //YOU WERE HERE
+    {
+        int tempPos = position;
+        if(colour == secretCode[tempPos])
+        {
+            correct++;
+        }
+        else
+        {
+            while() //There is a dillema here but I need to sleep or I am going to collapse
+        }
+
+
+
+    }
+
     public void receiveButtonPress(int colour)
     {
-        if(position == secretCodeLenght-1)
+        if(position == secretCodeLenght-1)  //If this is true than we finish
         {
-            
+
+
+
+
+
+
+
+
+
         }
         else
         {
             if()
             {
-
+                labels[position] =
             }
-            labels[position] = 
+ 
         }
     }
 
@@ -61,11 +88,11 @@ public class GameScreen
     {
         return(correct);
     }
-    int getHalfCorrect()
+    public int getHalfCorrect()
     {
         return(halfCorrect);
     }
-    int getPosition()
+    public int getPosition()
     {
         return(position);
     }
--- SCC-110 Week 18 Assesed Exercise/ResultScreen.java
+++ SCC-110 Week 18 Assesed Exercise/ResultScreen.java
@@ -8,6 +8,7 @@ public class ResultScreen
     private JPanel resultPanel = new JPanel();
     private int secretCodeLenght;
     private int width;
+    private JLabel labels[];        //Array of icons
 
     public ResultScreen(int newSecretCodeLenght)
     {
@@ -21,8 +22,14 @@ public class ResultScreen
         {
             width = secretCodeLenght;
         }
-        
+
         resultPanel.setLayout(new GridLayout(2,width));
+
+        for(int i=0; i<secretCodeLenght; i++)
+        {
+            labels[i] = new JLabel(new Picture("Empty.png"));
+            resultPanel.add(labels[i]);
+        }
     }
 
     public JPanel getResultScreen(int correct,int halfCorrect)
