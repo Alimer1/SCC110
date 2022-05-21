03f0363987e1f25fb3a8fe6e5167ce60355d3a63
Ali Omer Misirli <aliomermsrl@gmail.com>
2022-05-21 19:52:07 +0100

    Commit 1: I got a ball bouncing around finally figured it out how to make sleep function

--- SCC-110 Week 25 Project/Main.java
+++ SCC-110 Week 25 Project/Main.java
@@ -1,43 +1,46 @@
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
 
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         GameArena mainGame = new GameArena(1000,500);
-        Ball ball = new Ball(500,250,10,"BLUE");
+        Ball ball = new Ball(500,250,50,"BLUE");
 
         Line topLine = new Line(100,100,900,100,5,"RED");
         Line rightLine = new Line(900,100,900,400,5,"RED");
         Line bottomLine = new Line(900,400,100,400,5,"RED");
         Line leftLine = new Line(100,400,100,100,5,"RED");
-        Object test = new Object();
-        TimeUnit help = new TimeUnit();
+        Text text = new Text("Counter:",20,500,150,"ORANGE");
+        int counter = 0;
 
         mainGame.addBall(ball);
         mainGame.addLine(topLine);
         mainGame.addLine(rightLine);
         mainGame.addLine(bottomLine);
         mainGame.addLine(leftLine);
+        mainGame.addText(text);
 
-        double ballXSpeed = 0.01;
-        double ballYSpeed = 0.01;
+        double ballXSpeed = 1;
+        double ballYSpeed = 1;
 
         boolean gameOver = false;
 
         while(gameOver == false)
         {
+
+            if(ball.getYPosition()>400-25||ball.getYPosition()<100+25)
+            {
+                ballYSpeed = ballYSpeed * -1;
+            }
+            if(ball.getXPosition()>900-25||ball.getXPosition()<100+25)
+            {
+                ballXSpeed = ballXSpeed * -1;
+            }
             ball.setXPosition(ball.getXPosition()+ballXSpeed);
             ball.setYPosition(ball.getYPosition()+ballYSpeed);
-            help.SECONDS.sleep(1);
+            counter++;
+            text.setText("Counter:"+counter);
+            Thread.sleep(16);
         }
-
-
-    }
-
-    void Update()
-    {
-
     }
 }
