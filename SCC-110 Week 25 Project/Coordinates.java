9cb7180e435fc2def6c2f411bcfe98b5e84536a9
Ali Omer Misirli <aliomermsrl@gmail.com>
2022-05-22 04:02:04 +0100

    Commit 6: Finally finished the line collision code there is still some fixing needed but it seems to work tommorrow I will need to rush through the tasks. I need to sleep.

--- SCC-110 Week 25 Project/Coordinates.java
+++ SCC-110 Week 25 Project/Coordinates.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class Coordinates
 {
 
@@ -10,6 +12,39 @@ public class Coordinates
         x = nx;
         y = ny;
     }
+
+    double distanceFromOrigin()
+    {
+        return(Math.sqrt((x*x)+(y*y)));
+    }
+
+    void setX(double nx)
+    {
+        x = nx;
+    }
+
+    void setY(double ny)
+    {
+        y = ny;
+    }
+
+    double getX()
+    {
+        return(x);
+    }
+
+    double getY()
+    {
+        return(y);
+    }
+
+    static Coordinates ballToCoordinates(Ball ball)
+    {
+        Coordinates result = new Coordinates(ball.getXPosition(), ball.getYPosition());
+        return(result);
+    }
+
+
 
     static boolean lineBallColision(Ball ball,Line line)
     {
@@ -41,7 +76,6 @@ public class Coordinates
     {
         double x0 = ball.getXPosition();
         double y0 = ball.getYPosition();
-        double r = ball.getSize()/2;
         double x1 = line.getXStart();
         double y1 = line.getYStart();
         double x2 = line.getXEnd();
@@ -52,6 +86,15 @@ public class Coordinates
         
         return(a/b);
     }
+
+    static double linePointDistance(double x0,double y0, double x1, double y1, double x2, double y2) // (0) = Point   (1,2) = Line
+    {
+        double a = Math.abs(((x2-x1)*(y1-y0))-((x1-x0)*(y2-y1)));
+        double b = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+        
+        return(a/b);
+    }
+
 
     static double[] lineBallClosestPoint(Ball ball,Line line,double d)
     {
@@ -72,6 +115,57 @@ public class Coordinates
         double[] closestPoint = {x3,y3};
 
         return(closestPoint);
+    }
+
+    static double[] linePointClosestPoint(double x0,double y0, double x1, double y1, double x2, double y2,double d) // (0) = Point   (1,2) = Line
+    {
+        double a = Math.sqrt(((x1-x0)*(x1-x0))+((y1-y0)*(y1-y0))); //Distance from one corner of the line to the ball
+        double b = Math.sqrt((a*a)-(d*d));                         //Distance from that corner to the closest point on the line
+        double c = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))); //Distance from the two point of the line
+        double ratio = b/c;                                        //Ratio of the part of the line to the whole of it
+        double x3 = ((x2-x1)*ratio)+x1;
+        double y3 = ((y2-y1)*ratio)+y1;
+        
+        double[] closestPoint = {x3,y3};
+
+        return(closestPoint);
+    }
+
+    static double[] speedAfterCollision(double x0,double y0,double x1,double y1)   //x0,y0 = Speed Vector x1,y1 = Closest Point On the Line
+    {
+        double speedVector[] = {x0,y0};
+        double distance = linePointDistance(x0, y0, x1, y1, 0, 0);
+        double deletedVector[] = linePointClosestPoint(x0, y0, x1, y1, 0, 0, distance);
+        speedVector[0] = deletedVector[0];
+        speedVector[1] = deletedVector[1];
+        return(speedVector);
+    }
+
+    static double angleBetween (Coordinates v0,Coordinates v1)  //Calculating the angle between two Vectors
+    {
+        double a = (v0.getX()*v1.getX())+(v0.getY()*v1.getY());
+        double b = Math.sqrt((v0.getX()*v0.getX())+(v0.getY()*v0.getY()));
+        double c = Math.sqrt((v1.getX()*v1.getX())+(v1.getY()*v1.getY()));
+        double angle = Math.acos(a/(b*c));
+        return(angle);
+    }
+
+    static Coordinates remainingVelocityVector(double angle,Coordinates v1,Coordinates v2)  //v1 = line v2 = speed
+    {
+        double Line = v1.distanceFromOrigin();
+        //System.out.println("ugghhh:"+v1.getX()+"/"+v1.getY());
+        //System.out.println(Line);
+        double cos = Math.cos(angle);
+        //System.out.println(cos);
+        double sLine = cos * v2.distanceFromOrigin();
+        //System.out.println(sLine);
+        double ratio = sLine/Line;
+        System.out.println(ratio);
+        Coordinates f0 = new Coordinates(v1.getX()*ratio,v1.getY()*ratio);
+        Coordinates answer = new Coordinates(v2.getX()-f0.getX(),v2.getY()-f0.getY());
+        System.out.println("X:"+v1.getX());
+        System.out.println("Y:"+v1.getY());
+        return(answer);
     }
 
 }
--- SCC-110 Week 25 Project/Main.java
+++ SCC-110 Week 25 Project/Main.java
@@ -11,53 +11,64 @@ public class Main
             player[i] = new Player(i);
             mainGame.addBall(player[i].getBall());
         }
-        for(int i=0;i<4;i++)
-        {
-            wall[i] = new Line(100, 100+(i*50), 800, 150+(i*50), 5, "RED");
-            mainGame.addLine(wall[i]);
-        }
+
+        wall[0] = new Line(100, 100, 900, 100, 5, "RED");
+        wall[1] = new Line(900, 100, 900, 400, 5, "RED");
+        wall[2] = new Line(900, 400, 100, 400, 5, "RED");
+        wall[3] = new Line(100, 400, 100, 100, 5, "RED");
 
 
-        double ballXSpeed = 0;
-        double ballYSpeed = 0;
+        mainGame.addLine(wall[0]);
+        mainGame.addLine(wall[1]);
+        mainGame.addLine(wall[2]);
+        mainGame.addLine(wall[3]);
+
+        Coordinates velocity = new Coordinates(0, 0);
 
         boolean gameOver = false;
 
         while(gameOver == false)    //Main loop of the program that triggers every second until game over
         {
 
-            ballXSpeed = 0;
-            ballYSpeed = 0;
+            velocity.setX(0);
+            velocity.setY(0);
 
             if(mainGame.upPressed()==true)
             {
-                ballYSpeed = -3;
+                velocity.setY(-3);
             }
             if(mainGame.downPressed()==true)
             {
-                ballYSpeed = 3;
+                velocity.setY(3);
             }
             if(mainGame.rightPressed()==true)
             {
-                ballXSpeed = 3;
+                velocity.setX(3);
             }
             if(mainGame.leftPressed()==true)
             {
-                ballXSpeed = -3;
+                velocity.setX(-3);
             }
 
 
             boolean c = Coordinates.lineBallColision(player[0].getBall(), wall[0]);
             double d = Coordinates.lineBallDistance(player[0].getBall(), wall[0]);
             double cp[] = Coordinates.lineBallClosestPoint(player[0].getBall(), wall[0], d);
+            Coordinates closePoint = new Coordinates(cp[0], cp[1]);
+            Coordinates closePointFromPlayer = new Coordinates(closePoint.getX()-player[0].getX(),closePoint.getY()-player[0].getY());
 
             wall[1].setLinePosition(player[0].getX(),player[0].getY(), cp[0], cp[1]);
 
-            player[0].setX(player[0].getX()+ballXSpeed);
-            player[0].setY(player[0].getY()+ballYSpeed);
+            if(c==true&&!(velocity.getX()==0&&velocity.getY()==0))
+            {
+                velocity = Coordinates.remainingVelocityVector(Coordinates.angleBetween(closePointFromPlayer,velocity), closePointFromPlayer, velocity);
+            }
 
-
-            Thread.sleep(16);
+            player[0].setX(player[0].getX()+velocity.getX());
+            player[0].setY(player[0].getY()+velocity.getY());
+            System.out.println("Is c true:"+c);
+            System.out.println("Velocity: X("+velocity.getX()+") Y("+velocity.getY()+")");
+            Thread.sleep(100);
         }
     }
 }
